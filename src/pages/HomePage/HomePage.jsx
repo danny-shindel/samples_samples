0a3f5988bf550703a48bd4341f77dca74afeb2d9
Chase Anderson <anderson.e.chase@gmail.com>
2021-06-10 12:24:51 -0700

    first stage search wines

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,3 @@
-import * as userService from '../../utilities/users-service';
 import { Link, useHistory } from "react-router-dom"
 import { useState } from 'react';
 import * as winesAPI from "../../utilities/wines-api";
@@ -7,6 +6,9 @@ import * as winesAPI from "../../utilities/wines-api";
 export default function HomePage({ setWine, wineTitles }) {
   const [search, setSearch] = useState({ title: '' });
   const history = useHistory();
+  // state for autocomplete feature
+  const [display, setDisplay] = useState(false);
+
   
   function handleChange(evt) {
     setSearch({[evt.target.name]: evt.target.value});
@@ -23,10 +25,44 @@ export default function HomePage({ setWine, wineTitles }) {
     }
   }
 
+  // Helper Functions
+  function setSearchedWine(wine) {
+    setSearch(wine);
+    setDisplay(false);
+  }
+
   return (
     <>
       <h1>HOMEPAGE</h1>
-      <input type="text" name="title" onChange={handleChange} value={search.title} required/>
+      <div className="dropdown is-active">
+        <div className="dropdown-trigger">
+          <input 
+          type="text" 
+          name="title" 
+          onChange={handleChange} 
+          onClick={() => setDisplay(!display)} 
+          value={search.title} 
+          required
+          />
+        </div>
+        <div className="dropdown-menu">
+          { display && (
+            <div className="dropdown-content">
+              { wineTitles
+                .filter(({ title }) => title.toLowerCase().includes(search.title.toLowerCase()))
+                .map((wine, idx) => {
+                    return(
+                      <div key={idx}>
+                        <div className="dropdown-item" onClick={() => setSearchedWine(wine)}>{ wine.title }</div>
+                        <hr className="dropdown-divider" />
+                      </div>
+                    )
+                  }) 
+                }
+            </div>
+                ) }
+        </div>
+      </div>
       <button onClick={handleSubmit}> Submit </button>
       <br/>
       <Link to="/results">
