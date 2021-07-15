1dbe0e7e6cc51e1177ca605a3ecd4b0f9d0dcba0
Erwin Medina <erwin.l.medina@gmail.com>
2021-07-15 16:44:20 -0700

    lots more css. stills needs responsiveness; worked on results page

--- src/pages/ResultsPage/ResultsPage.jsx
+++ src/pages/ResultsPage/ResultsPage.jsx
@@ -57,47 +57,69 @@ export default function ResultsPage({ user, setClick, wine, setWine, allWines, s
     if (trueSeconds < 10) {
       trueSeconds = '0' + trueSeconds;
     }
-    return (minutes +":" + trueSeconds);
+    return (minutes + ":" + trueSeconds);
 
   }
 
   return (
     <div className="resultsPage">
-      <div className="columns">
-        <div className="leftHand column is-5">
-          <img src="https://i.imgur.com/8upb3GM.png" alt="wine bottle"></img>
-          <p>
-            {wine && wine.title}
-          </p>
-          {user ?
-            <>
-              <Link to='/create'><button onClick={handleCreateClick}>Create Playlist</button></Link>
-            </>
-            :
-            <>
-              <Link to='/auth'><button onClick={() => setClick(2)}>Log In To Create Playlist</button></Link>
-            </>
-          }
+      <div className="resultsPageContainer">
+        <div className="leftHand">
+          
+          <p className="wineTitle">{wine && wine.title}</p>
+          <div className="resultsPageLeftHandImage">
+            <img src="https://i.imgur.com/8upb3GM.png" alt="wine bottle"></img>
+            <div className="resultsPageImageDetails">
+              <p>{wine && wine.variety}</p>
+              <p>Region: {wine &&  wine.region_1}</p>
+              <p>Province: {wine && wine.province}</p>
+            </div>
+          </div>
+          
+          <div className="resultsPageWineDetails">
+            <p>{wine && wine.description}</p>
+            {user ?
+              <>
+                <Link to='/create'><button onClick={handleCreateClick}>Create Playlist</button></Link>
+              </>
+              :
+              <>
+                <Link to='/auth'><button onClick={() => setClick(2)}>Log In To Create Playlist</button></Link>
+              </>
+            }
+          </div>
+
         </div>
-        <div className="rightHand column is-7">
+        <div className="rightHand">
           {wine && wine.playlists.map(playlist => (
-            <>
+            <div className="rightHandContainer">
               <div className="playlist-holder">
                 <div className="playlist-info">
-                  <img src={playlist.user.profilePic} className="playlist-profile" />
-                  <div className="playlist-title">{playlist.title}</div>
-                  <div className="playlist-user">{playlist.user.name} | <span className="playlistDownloads">{playlist.saved.length} downloads</span></div>
-                  {myPlaylistPage ?
-                    <button className="button playlist-button" onClick={() => editPlaylist(playlist)}>
-                      Edit
-                    </button>
-                    :
-                    <>
-                    { playlist.user._id !== user._id && <button className="button playlist-button" onClick={() => savePlaylist(playlist._id)}>
-                        {playlist.saved.find(userId => userId === user._id) ? 'unsave' : 'save'}
-                      </button>}
-                    </>
-                  }
+                  <div className="playlist-info-userImage">
+                    <img src={playlist.user.profilePic} className="playlist-profile" />
+                  </div>
+                  <div className="playlist-info-name">
+                    <p className="playlist-title">{playlist.title}</p>
+                    <p className="playlist-user">
+                      {playlist.user.name} | <span className="playlistDownloads">{playlist.saved.length} downloads</span>
+                    </p>
+                  </div>
+                  <div className="playlist-info-button">
+                    {myPlaylistPage ?
+                      <button className="button playlist-button" onClick={() => editPlaylist(playlist)}>
+                        <span><i class="fas fa-edit icons"></i>Edit</span> 
+                      </button>
+                      :
+                      <>
+                      { playlist.user._id !== user._id && <button className="button playlist-button" onClick={() => savePlaylist(playlist._id)}>
+                          {playlist.saved.find(userId => userId === user._id) ? 
+                            <span><i class="fas fa-star icons"></i>Unsave</span> 
+                            : 
+                            <span><i class="far fa-star icons"></i>Save</span>}
+                        </button>}
+                      </>
+                    }
+                  </div>
                 </div>
                 <div className="playlist-songs">
                   <div className="song-holder">
@@ -107,18 +129,15 @@ export default function ResultsPage({ user, setClick, wine, setWine, allWines, s
                           <img src={song.album.cover_small} alt="" />
                           <h3>{song.title} - {song.artist.name}</h3>
                         </div>
-                        
                         <div className="playlistHolderSongLength">
-                          {console.log(song)}
                           <span>{handleSongLength(song.duration)}</span>
                         </div>
-
                       </div>
                     ))}
                   </div>
                 </div>
               </div>
-            </>
+            </div>
           ))}
         </div>
       </div>
